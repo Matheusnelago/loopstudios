e3749ef3e0b77777068a656392a9ab677d6b8272
Matheus Nelago <130694676+Matheusnelago@users.noreply.github.com>
2024-02-25 13:37:40 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ let open = document.querySelector('.ham').addEventListener('click', (e) => {
     e.preventDefault();
     nav.style.display = 'block';
     body.style.overflowY = 'hidden';
+    nav.style.transition = 'all .5s ease-in-out';
 });
 let close = document.querySelector('.close').addEventListener('click', (e) => {
     e.preventDefault();
     nav.style.display = 'none';
     body.style.overflowY = 'auto';
+    nav.style.transition = 'all .5s ease-in-out';
 });
 
 window.addEventListener('resize', (e) => {
